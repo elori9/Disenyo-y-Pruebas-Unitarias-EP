d03d18bd65ed17a7f20a7f7c954fe5e61929054e
jgb28 <jgbstick@gmail.com>
2025-12-12 16:19:01 +0100

    DigitalSignature PacketData and modified git ignore

--- src/main/java/data/DigitalSignature.java
+++ src/main/java/data/DigitalSignature.java
@@ -1,4 +1,49 @@
 package data;
 
+import exceptions.DigitalSignatureException;
+
+import java.util.Arrays;
+
 public class DigitalSignature {
+
+
+    private final byte[] digitalSignature;
+
+    /**
+     * Constructor of the class
+     *
+     * @param signature the Signature
+     * @throws DigitalSignatureException if the code is null or the code has invalid format
+     */
+    public DigitalSignature(byte[] signature) throws DigitalSignatureException {
+        if (signature == null)
+            throw new DigitalSignatureException("DigitalSignature code can't be null");
+
+        if (signature.length != 16)
+            throw new DigitalSignatureException("DigitalSignature must be 16 characters long");
+
+        this.digitalSignature = signature;
+    }
+
+    public byte[] getDigitalSignature() {
+        return digitalSignature;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DigitalSignature dSignature = (DigitalSignature) o;
+        return Arrays.equals(dSignature.digitalSignature, digitalSignature);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(digitalSignature);
+    }
+
+    @Override
+    public String toString() {
+        return "DigitalSignature{" + "Signature='" + Arrays.toString(digitalSignature) + '\'' + '}';
+    }
 }
--- src/test/java/data/DigitalSignatureTest.java
+++ src/test/java/data/DigitalSignatureTest.java
@@ -1,4 +1,43 @@
 package data;
 
+import exceptions.DigitalSignatureException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class DigitalSignatureTest {
+
+    private DigitalSignature digitalSignature;
+
+    @Test
+    @DisplayName("Test throws DigitalSignatureException for null code")
+    void nullCode() {
+        Assertions.assertThrows(DigitalSignatureException.class, () -> {
+            digitalSignature = new DigitalSignature(null);
+        });
+    }
+
+    @Test
+    @DisplayName("Test throws DigitalSignatureException for non 16 characters length")
+    void invalidLength() {
+        // Less than 16 chars
+        Assertions.assertThrows(DigitalSignatureException.class, () -> {
+            digitalSignature = new DigitalSignature("1".getBytes());
+        });
+        // More than 16 chars
+        Assertions.assertThrows(DigitalSignatureException.class, () -> {
+            byte[] bytes = new byte[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+            digitalSignature = new DigitalSignature(bytes);
+        });
+    }
+
+    @Test
+    @DisplayName("Valid Signature")
+    void validCode() throws DigitalSignatureException {
+        byte[] bytes = new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 'A', 'B', 'C', 'D', 'E', 'G', 'H', 'J'};
+        digitalSignature = new DigitalSignature(bytes);
+
+        Assertions.assertEquals(bytes, digitalSignature.getDigitalSignature());
+    }
 }
