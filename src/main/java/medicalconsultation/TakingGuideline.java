ff22f3e447f6573306d11c2d10af30d3a5c61afb
elori9 <originaly55@gmail.com>
2025-12-13 18:43:37 +0100

    takingguideline class + test, and enum dayMoment, and IncorrectTakingGuidelineException

--- src/main/java/medicalconsultation/TakingGuideline.java
+++ src/main/java/medicalconsultation/TakingGuideline.java
@@ -1,4 +1,69 @@
 package medicalconsultation;
 
+import exceptions.IncorrectTakingGuidelinesException;
+import exceptions.PosologyException;
+
 public class TakingGuideline {
+    private dayMoment dMoment;
+    private float duration;
+    private Posology posology;
+    private String instructions;
+
+    /**
+     * Constructor.
+     *
+     * @param dM dayMoment
+     * @param du duration
+     * @param d  Dose
+     * @param f  Frequency
+     * @param fu Frequency Unit
+     * @param i  instructions
+     * @throws IncorrectTakingGuidelinesException if the day moment or instructions are null, or the duration is < 0
+     * @throws PosologyException                  if the dose, frequency are < 0, or the frequency unit is null
+     */
+    public TakingGuideline(dayMoment dM, float du, float d, float f,
+                           FqUnit fu, String i) throws IncorrectTakingGuidelinesException, PosologyException {
+        this.setDMoment(dM);
+        this.setDuration(du);
+        this.setInstructions(i);
+        this.posology = new Posology(d, f, fu);
+    }
+
+
+    public dayMoment getDMoment() {
+        return dMoment;
+    }
+
+    public float getDuration() {
+        return duration;
+    }
+
+    public Posology getPosology() {
+        return posology;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setDMoment(dayMoment dMoment) throws IncorrectTakingGuidelinesException {
+        if (dMoment == null) throw new IncorrectTakingGuidelinesException("DayMoment can't be null");
+        this.dMoment = dMoment;
+    }
+
+    public void setDuration(float duration) throws IncorrectTakingGuidelinesException {
+        if (duration < 0) throw new IncorrectTakingGuidelinesException("Duration can't be negative");
+        this.duration = duration;
+    }
+
+    public void setPosology(Posology posology) throws PosologyException {
+        if (posology == null) throw new PosologyException("Posology can not be null");
+        this.posology = posology;
+    }
+
+    public void setInstructions(String instructions) throws IncorrectTakingGuidelinesException {
+        if (instructions == null) throw new IncorrectTakingGuidelinesException("Instructions can't be null");
+        this.instructions = instructions;
+    }
+
 }
--- src/test/java/medicalconsultation/TakingGuidelineTest.java
+++ src/test/java/medicalconsultation/TakingGuidelineTest.java
@@ -1,4 +1,63 @@
 package medicalconsultation;
 
+import exceptions.IncorrectTakingGuidelinesException;
+import exceptions.PosologyException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TakingGuidelineTest {
+
+    @Test
+    @DisplayName("Test verifies that the creations are correct")
+    void testValidTakingGuidelineCreation() throws PosologyException, IncorrectTakingGuidelinesException {
+        TakingGuideline takingGuideline = new TakingGuideline(dayMoment.DURINGBREAKFAST, 3, 10, 2, FqUnit.DAY, "instruction");
+
+        assertEquals(dayMoment.DURINGBREAKFAST, takingGuideline.getDMoment());
+        assertEquals(3, takingGuideline.getDuration());
+        assertEquals("instruction", takingGuideline.getInstructions());
+        assertNotNull(takingGuideline.getPosology());
+    }
+
+    @Test
+    @DisplayName("Test verifies the validation of the setters")
+    void testValidSetters() throws IncorrectTakingGuidelinesException, PosologyException {
+        TakingGuideline takingGuideline = new TakingGuideline(dayMoment.DURINGBREAKFAST, 3, 10, 2, FqUnit.DAY, "instruction");
+        Posology posology = new Posology(10, 2, FqUnit.DAY);
+        takingGuideline.setDMoment(dayMoment.DURINGBREAKFAST);
+        takingGuideline.setDuration(2);
+        takingGuideline.setPosology(posology);
+        takingGuideline.setInstructions("i");
+
+        assertEquals(dayMoment.DURINGBREAKFAST, takingGuideline.getDMoment());
+        assertEquals(2, takingGuideline.getDuration());
+        assertEquals(posology, takingGuideline.getPosology());
+        assertEquals("i", takingGuideline.getInstructions());
+    }
+
+    @Test
+    @DisplayName("Test throws IncorrectTakingGuidelinesException if the day moment is null")
+    void testNullDayMomentThrowsException() {
+        assertThrows(IncorrectTakingGuidelinesException.class, () -> {
+            new TakingGuideline(null, 3, 10, 2, FqUnit.DAY, "instruction");
+        });
+    }
+
+    @Test
+    @DisplayName("Test throws IncorrectTakingGuidelinesException for negative duration")
+    void testNegativeDurationThrowsException() {
+        assertThrows(IncorrectTakingGuidelinesException.class, () -> {
+            new TakingGuideline(dayMoment.DURINGBREAKFAST, -1, 10, 2, FqUnit.DAY, "instruction");
+        });
+    }
+
+    @Test
+    @DisplayName("Test throws IncorrectTakingGuidelinesException for null instructions")
+    void testNullInstructionsThrowsException() {
+        assertThrows(IncorrectTakingGuidelinesException.class, () -> {
+            new TakingGuideline(dayMoment.DURINGBREAKFAST, 1, 10, 2, FqUnit.DAY, null);
+        });
+    }
+
 }
